ac70f8f5279d7738424d5045215708b5a8ec1e5f
Ki-Jana Carter <kijana@mit.edu>
2020-03-26 09:13:05 -0400

    refactor: Remove extraneous comments and commented code. Add type hinting imports. Rewrite and fix WAVE. Rewrite Module and remove Module.transform. Rewrite FC. Remove all Module.transform decorators.

--- src/luz/modules.py
+++ src/luz/modules.py
@@ -1,273 +1,26 @@
-import torch
-
 """
 
 Custom PyTorch modules. Each class must implement torch.nn.Module, and therefore must have an __init__ method and a forward method.
-Additionally, for consistency with the remainder of luz, each class must have a plot_history function. While this can be custom defined,
-it is recommended that each custom module class simply inherit plot_history from the Module class defined here.
 
 """
+from __future__ import annotations
+from typing import Iterable, Optional, Tuple
+
+import collections
+import networkx as nx
+import torch
+
+__all__ = ["ElmanRNN", "FC", "FCRNN", "Module", "Reshape", "WAVE"]
 
 
 class Module(torch.nn.Module):
-    def __init__(self, module, input_transform=None, output_transform=None):
+    def __init__(self, module: torch.nn.Module, activation=None) -> None:
         super().__init__()
-
         self.module = module
+        self.activation = activation or (lambda x: x)
 
-        self.input_transform = input_transform
-        self.output_transform = output_transform
-
-    def transform(fn):
-        def transformed_forward(self, x):
-            # if transform_input, we must be in evaluation/prediction stage, which means data is being batched before it is transformed
-            # however, transforms are written to apply to unbatched data, so this will cause the transform to see data with an extra dimension
-            # in this case, the data must be applied to each element in the batch sequentially, then recombined
-            input = (
-                torch.stack(
-                    tensors=[self.input_transform.transform(y) for y in x], dim=0
-                )
-                if self.input_transform
-                else x
-            )
-
-            # output = self.output_transform.forward(fn(self,input)) if self.output_transform else fn(self,input)
-            output = (
-                self.output_transform(fn(self, input))
-                if self.output_transform is not None
-                else fn(self, input)
-            )
-            return output
-
-        transformed_forward.untransformed = fn
-
-        return transformed_forward
-
-    # @transform
-    # def forward(self, x):
-    #     raise NotImplementedError('The function forward must be overwritten for this class.')
-    @transform
-    def forward(self, x):
-        return self.module.forward(x)
-
-    def predict(self, inputs):  # inputs should be a dataset object
-        assert False, "TODO: Fix/flesh out predict (using forward)."
-        raise NotImplementedError(
-            "The function predict must be overwritten for this class."
-        )
-
-    def _calculate_size(self, input_size):
-        with torch.no_grad():
-            x = torch.randn(input_size).unsqueeze(0)
-            output = self.forward(x)
-        return output.size()[1:]
-
-
-class WAVE(torch.nn.Module):
-    def __init__(self, atom_feature_size, atom_out_size, num_passes):
-        super(WAVE, self).__init__()
-
-        self.atom_feature_size = atom_feature_size
-        self.num_passes = num_passes
-
-        self.T_to_A = torch.nn.Linear(
-            in_features=2 * self.atom_feature_size, out_features=self.atom_feature_size
-        )
-        self.C_to_A = torch.nn.Linear(
-            in_features=2 * self.atom_feature_size, out_features=self.atom_feature_size
-        )
-        self.T_to_B = torch.nn.Linear(
-            in_features=2 * self.atom_feature_size, out_features=self.atom_feature_size
-        )
-        self.C_to_B = torch.nn.Linear(
-            in_features=2 * self.atom_feature_size, out_features=self.atom_feature_size
-        )
-        self.W = torch.randn(size=(self.atom_feature_size, 1))
-        self.W_u = torch.nn.Linear(
-            in_features=2 * self.atom_feature_size, out_features=self.atom_feature_size
-        )
-        self.W_o = torch.nn.Linear(
-            in_features=2 * self.atom_feature_size, out_features=self.atom_feature_size
-        )
-
-        self.sigmoid = torch.nn.Sigmoid()
-        self.relu = torch.nn.ReLU()
-
-        self.atom_output = torch.nn.Sequential(
-            torch.nn.Linear(in_features=self.atom_feature_size, out_features=10),
-            torch.nn.Linear(in_features=10, out_features=atom_out_size),
-        )
-
-        self.bond_output = None
-
-    def _propagate(self, digraph, pass_type):
-        if pass_type == "out":
-            self.tree_nodes = []
-            self.cross_nodes = [self.root]
-            self.next_nodes = list(digraph.successors(self.root))
-        elif pass_type == "in":
-            self.tree_nodes = []
-            self.next_nodes = [
-                p for c in self.cross_nodes for p in list(digraph.predecessors(c))
-            ]
-
-        while len(self.next_nodes) > 0:
-            self._move_along_graph(digraph=digraph, pass_type=pass_type)
-
-            C = torch.narrow(
-                self.atom_state_tensor,
-                dim=1,
-                start=self.index_list.index(self.cross_nodes[0]),
-                length=len(self.cross_nodes),
-            )
-            T = torch.narrow(
-                self.atom_state_tensor,
-                dim=1,
-                start=self.index_list.index(self.tree_nodes[0]),
-                length=len(self.tree_nodes),
-            )
-            N = torch.narrow(
-                self.atom_state_tensor,
-                dim=1,
-                start=self.index_list.index(self.cross_nodes[0]),
-                length=len(self.cross_nodes) + len(self.tree_nodes),
-            )
-
-            for c in self.cross_nodes:
-                s = torch.narrow(
-                    self.atom_state_tensor,
-                    dim=1,
-                    start=self.index_list.index(c),
-                    length=1,
-                )
-
-                m = self._mix_gate(state=s, tree=T, cross=C, neighbors=N)
-
-                u = self._update_gate(state=s, mix=m)
-                o = self._output_gate(state=s, mix=m)
-
-                s += (
-                    self._update_state(update=u, output=o, mix=m) - s
-                )  # this is a hacky trick to update s in place so that the data in atom_state_tensor gets updated as well
-
-    def _move_along_graph(self, digraph, pass_type):
-        self.tree_nodes = self.cross_nodes
-        self.cross_nodes = self.next_nodes
-        if pass_type == "out":
-            self.next_nodes = list(
-                collections.OrderedDict.fromkeys(
-                    [r for s in self.next_nodes for r in list(digraph.predecessors(s))]
-                ).keys()
-            )
-        elif pass_type == "in":
-            self.next_nodes = list(
-                collections.OrderedDict.fromkeys(
-                    [r for s in self.next_nodes for r in list(digraph.successors(s))]
-                ).keys()
-            )
-
-    def _mix_gate(self, state, tree, cross, neighbors):
-        input_T = torch.t(torch.cat([tree, state.expand_as(tree)], dim=0))
-        input_C = torch.t(torch.cat([cross, state.expand_as(cross)], dim=0))
-
-        A = torch.nn.functional.softmax(
-            torch.cat(
-                tensors=[torch.t(self.T_to_A(input_T)), torch.t(self.C_to_A(input_C))],
-                dim=1,
-            ),
-            dim=1,
-        ) * self.W.expand_as(neighbors)
-        B = torch.nn.functional.softsign(
-            torch.cat(
-                tensors=[torch.t(self.T_to_B(input_T)), torch.t(self.C_to_B(input_C))],
-                dim=1,
-            )
-        )
-
-        return torch.sum((A + B) * neighbors, dim=1).unsqueeze(1)
-
-    def _update_gate(self, state, mix):
-        return self.sigmoid(
-            torch.t(self.W_u(torch.t(torch.cat(tensors=[mix, state], dim=0))))
-        )
-
-    def _output_gate(self, state, mix):
-        return self.relu(
-            torch.t(self.W_o(torch.t(torch.cat(tensors=[mix, state], dim=0))))
-        )
-
-    def _update_state(self, update, output, mix):
-        return update * output + (1 - update) * mix
-
-    @Module.transform
-    def forward(self, x):
-        self.root = next(nx.topological_sort(x))
-
-        self.index_list = [s for s in x]
-        self.atom_state_dict = nx.get_node_attributes(x, "props")
-        # the atom states vectors are stored in a numpy array so that they can be accessed repeatedly and cast to tensors with minimal memory overhead
-        self.atom_state_tensor = torch.t(
-            torch.tensor([self.atom_state_dict[s] for s in x])
-        ).float()
-
-        for n in range(self.num_passes):
-            self._propagate(digraph=x, pass_type="out")
-            self._propagate(digraph=x, pass_type="in")
-
-        return torch.t(self.atom_output(torch.t(self.atom_state_tensor)))
-
-
-class FCRNN(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super().__init__()
-
-        self.hidden_size = hidden_size
-        self.hidden = self._init_hidden()
-
-        self.i2h = torch.nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = torch.nn.Linear(input_size + hidden_size, output_size)
-
-    @Module.transform
-    def forward(self, x):
-        self.hidden = self._init_hidden()
-
-        for i in x.transpose(0, 1):
-            combined = torch.cat((i, self.hidden), 1)
-            self.hidden = self.i2h(combined)
-            output = self.i2o(combined)
-
-        return output
-
-    def _init_hidden(self):
-        return torch.zeros(1, self.hidden_size)
-
-
-class FC(torch.nn.Module):
-    def __init__(self, input_size, output_size, *hidden_sizes):
-        super().__init__()
-
-        if len(hidden_sizes) > 0:
-            layer_list = [
-                torch.nn.Linear(in_features=input_size, out_features=hidden_sizes[0])
-            ]
-            for i in range(len(hidden_sizes) - 1):
-                layer_list.append(
-                    torch.nn.Linear(
-                        in_features=hidden_sizes[i], out_features=hidden_sizes[i + 1]
-                    )
-                )
-            layer_list.append(
-                torch.nn.Linear(in_features=hidden_sizes[-1], out_features=output_size)
-            )
-        self.seq = torch.nn.Sequential(*layer_list)
-
-    @Module.transform
-    def forward(self, x):
-        return self.seq.forward(x).float()
-
-    def predict(self, x):
-        with torch.no_grad():
-            return self.seq.forward(x).float()
+    def forward(self, *args: torch.Tensor, **kwargs: torch.Tensor) -> torch.Tensor:
+        return self.activation(self.module(*args, **kwargs))
 
 
 class ElmanRNN(torch.nn.Module):
@@ -306,13 +59,50 @@ class ElmanRNN(torch.nn.Module):
             input_size=input_size, hidden_size=hidden_size, **kwargs
         )
 
-    @Module.transform
     def forward(self, x):
         if self.h0:
             output, hidden = self.rnn.forward(input=x, h0=self.h0)
         else:
             output, hidden = self.rnn.forward(input=x)
         return output
+
+
+class FC(torch.nn.Module):
+    def __init__(self, in_features, out_features, *hidden_features):
+        super().__init__()
+        sizes = tuple((in_features, *hidden_features, out_features))
+        layers = (
+            torch.nn.Linear(in_features=n_in, out_features=n_out)
+            for n_in, n_out in zip(sizes, sizes[1:])
+        )
+        self.seq = torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.seq(x)
+
+
+class FCRNN(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+        self.hidden = self._init_hidden()
+
+        self.i2h = torch.nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = torch.nn.Linear(input_size + hidden_size, output_size)
+
+    def forward(self, x):
+        self.hidden = self._init_hidden()
+
+        for i in x.transpose(0, 1):
+            combined = torch.cat((i, self.hidden), 1)
+            self.hidden = self.i2h(combined)
+            output = self.i2o(combined)
+
+        return output
+
+    def _init_hidden(self):
+        return torch.zeros(1, self.hidden_size)
 
 
 class Reshape(torch.nn.Module):
@@ -324,95 +114,98 @@ class Reshape(torch.nn.Module):
         return x.view(self.shape)
 
 
-# import torch
-# import torch_geometric
+class WAVE(torch.nn.Module):
+    def __init__(self, atom_feature_size: int, num_passes: int) -> None:
+        super().__init__()
 
-# class GCN(torch_geometric.nn.MessagePassing):
-#     def __init__(self, in_channels, out_channels):
-#         super().__init__(aggr='add')
-#         self.lin = torch.nn.Linear(in_features=in_channels,out_features=out_channels)
+        self.d = atom_feature_size
+        self.num_passes = num_passes
 
-#     def forward(self, data):
-#         x,edge_index = data.x,data.edge_index
-#         # x has shape [N, in_channels]
-#         # edge_index has shape [2, E]
+        self.T_to_A = torch.nn.Linear(in_features=2 * self.d, out_features=self.d)
+        self.C_to_A = torch.nn.Linear(in_features=2 * self.d, out_features=self.d)
+        self.T_to_B = torch.nn.Linear(in_features=2 * self.d, out_features=self.d)
+        self.C_to_B = torch.nn.Linear(in_features=2 * self.d, out_features=self.d)
+        self.W = torch.randn(size=(self.d, 1))
+        self.W_u = torch.nn.Linear(in_features=2 * self.d, out_features=self.d)
+        self.W_o = torch.nn.Linear(in_features=2 * self.d, out_features=self.d)
 
-#         # Step 1: Add self-loops to the adjacency matrix.
-#         edge_index, _ = torch_geometric.utils.add_self_loops(edge_index=edge_index,num_nodes=x.size(0))
+        self.sigmoid = torch.nn.Sigmoid()
+        self.relu = torch.nn.ReLU()
 
-#         # Step 2: Linearly transform node feature matrix.
-#         x = self.lin(x)
+    def forward(self, x: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
+        traversal_order = self._traverse_graph(x, edge_index)
 
-#         # Step 3-5: Start propagating messages.
-#         return self.propagate(edge_index, size=(x.size(0),x.size(0)),x=x)
+        lengths = tuple(len(l) for l in traversal_order)
+        perm = torch.eye(sum(lengths))[[a for b in traversal_order for a in b]]
 
-#     def message(self, x_j, edge_index, size):
-#         # x_j has shape [E, out_channels]
+        x = torch.matmul(perm, x).t()
 
-#         # Step 3: Normalize node features.
-#         row, col = edge_index
-#         deg = torch_geometric.utils.degree(index=row,num_nodes=size[0],dtype=x_j.dtype)
-#         deg_inv_sqrt = deg.pow(-0.5)
-#         norm = deg_inv_sqrt[row]*deg_inv_sqrt[col]
+        for _ in range(self.num_passes):
+            self._propagate(x, lengths)
+            self._propagate(x, lengths[::-1])
 
-#         return norm.view(-1, 1)*x_j
+        return x
 
-#     def update(self, aggr_out):
-#         # aggr_out has shape [N, out_channels]
+    def _traverse_graph(self, x: torch.Tensor, edge_index: torch.Tensor) -> Tuple[int]:
+        g = nx.Graph(list(edge_index.t().numpy()))
 
-#         # Step 5: Return new node embeddings.
-#         return aggr_out
+        root, *_ = nx.center(g)
 
-# # class GCN(torch.nn.Module):
-# #     def __init__(self, in_channels, out_channels):
-# #         super().__init__()
-# #         self.conv1 = _GCN(in_channels=in_channels,out_channels=5)
-# #         self.conv2 = _GCN(in_channels=5,out_channels=out_channels)
-# #         self.conv3 = _GCN(in_channels=5,out_channels=5)
-# #         self.conv4 = _GCN(in_channels=5,out_channels=out_channels)
-# #
-# #         self.relu = torch.nn.ReLU()
-# #
-# #     def forward(self, data):
-# #         #x, edge_index = data.x, data.edge_index
-# #
-# #         data.x = self.relu(self.conv1(data))
-# #         data.x = self.relu(self.conv2(data))
-# #         #data.x = self.relu(self.conv3(data))
-# #         #data.x = self.relu(self.conv4(data))
-# #         # print(data.x.shape)
-# #         # print(data)
-# #         return torch.topk(input=data.x,k=data.y.shape[0],dim=0)[0]#.unsqueeze(dim=0)
-# #         #return F.log_softmax(x, dim=1)
+        digraph = nx.bfs_tree(g, root)
 
+        order = [[root]]
+        while sum(len(a) for a in order) < len(g):
+            # FIXME: not sure what this ordered dict stuff actually does if anything...
+            succs = list(
+                collections.OrderedDict.fromkeys(
+                    [r for s in order[-1] for r in list(digraph.successors(s))]
+                ).keys()
+            )
+            order.append(succs)
 
-# # every model must have a state_dict function!
-# # if they inherit torch.nn.Module, then they already do...
-# class Model(torch.nn.Module):
-#     def __init__(self):
-#         super(Model, self).__init__()
-#         self.train_history = []
-#         self.val_history = []
-#
-#     @property
-#     def state_dict(self):
-#         return super().state_dict()
-#
-#     # # !!! redefine state_dict() and load_state_dict() to include plot history stuff?
-#     # def state_dict(self):
-#     #     import copy
-#     #     state_dict = super(Model, self).state_dict()
-#     #     state_dict['history'] = copy.deepcopy(self.train_history)
-#     #     return state_dict
-#     #
-#     # def load_state_dict(self, state_dict):
-#     #     import copy
-#     #
-#     #     self.train_history = state_dict.pop('history')
-#     #     super(Model, self).load_state_dict(state_dict)
-#     #     # !!! add the history back to the state_dict to avoid modifies state_dict; probably a bad solution but it does avoid copying entire state_dict
-#     #     state_dict['history'] = copy.deepcopy(self.train_history)
-#     #
-#     # def training_loss(self):
-#     #     return self.train_history[-1]
-#
+        return order
+
+    def _propagate(self, x: torch.Tensor, lengths: Iterable[int]) -> None:
+        offset = 0
+        for t, c in zip(lengths, lengths[1:]):
+            i = offset + t
+            j = i + c
+
+            T = x[:, offset:i]
+            C = x[:, i:j]
+            N = x[:, offset:j]
+
+            for k in range(c):
+                s = C.narrow(dim=1, start=k, length=1)
+
+                m = self._mix_gate(state=s, tree=T, cross=C, neighbors=N)
+
+                u = self.sigmoid(self.W_u(torch.cat(tensors=[m, s], dim=0).t()).t())
+                o = self.relu(self.W_o(torch.cat(tensors=[m, s], dim=0).t()).t())
+
+                C[:, k : k + 1] = u * o + (1 - u) * m
+
+            offset += t
+
+    def _mix_gate(
+        self,
+        state: torch.Tensor,
+        tree: torch.Tensor,
+        cross: torch.Tensor,
+        neighbors: torch.Tensor,
+    ) -> torch.Tensor:
+        input_T = torch.cat([tree, state.expand_as(tree)], dim=0).t()
+        input_C = torch.cat([cross, state.expand_as(cross)], dim=0).t()
+
+        A = torch.nn.functional.softmax(
+            torch.cat(
+                tensors=[self.T_to_A(input_T).t(), self.C_to_A(input_C).t()], dim=1,
+            ),
+            dim=1,
+        ) * self.W.expand_as(neighbors)
+
+        B = torch.nn.functional.softsign(
+            torch.cat([self.T_to_B(input_T).t(), self.C_to_B(input_C).t()], dim=1,)
+        )
+
+        return torch.sum((A + B) * neighbors, dim=1).unsqueeze(1)
