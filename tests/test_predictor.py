3f998ac90f2feead66f9f16cceafe6f934b008f7
Ki-Jana Carter <kijana@mit.edu>
2020-03-26 10:51:52 -0400

    style: Lint code

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -2,9 +2,10 @@ import luz
 import numpy as np
 import torch
 
+
 def test_predictor_trivial():
-    x = torch.tensor([0.])
+    x = torch.tensor([0.0])
 
-    fc = luz.Predictor(torch.nn.Linear(in_features=1, out_features=1,bias=False))
+    fc = luz.Predictor(torch.nn.Linear(in_features=1, out_features=1, bias=False))
 
-    np.testing.assert_allclose(fc.predict(x).detach().numpy(),np.array([0.]))
+    np.testing.assert_allclose(fc.predict(x).detach().numpy(), np.array([0.0]))
