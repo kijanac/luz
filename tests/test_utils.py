10d2db58f90c80cdc37a99b19231794ecde6bf85
Ki-Jana Carter <kijana@mit.edu>
2021-01-07 16:35:53 -0500

    test: Add tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,262 @@ import luz
 import torch
 
 
+def test_adjacency():
+    edge_index = torch.tensor(
+        [[0, 2], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 2]]
+    ).T
+    A = torch.tensor(
+        [
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 1, 0, 0, 0, 0],
+        ]
+    )
+
+    assert torch.allclose(A, luz.adjacency(edge_index))
+
+    edge_index = torch.cat((edge_index, edge_index.flipud()), dim=1)
+    A = torch.tensor(
+        [
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [1, 1, 0, 1, 0, 0, 1],
+            [0, 0, 1, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 0, 1, 0],
+        ]
+    )
+
+    assert torch.allclose(A, luz.adjacency(edge_index))
+
+
+def test_batchwise_mask():
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0])
+    m = torch.tensor([[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]])
+    assert torch.allclose(m, luz.batchwise_mask(batch))
+
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    m = torch.tensor(
+        [
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+        ]
+    )
+    assert torch.allclose(m, luz.batchwise_mask(batch))
+
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0])
+    edge_index = torch.tensor([[0, 0, 0, 0, 1, 1, 2], [1, 2, 3, 4, 5, 6, 8]])
+    m = torch.tensor([[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]])
+    assert torch.allclose(m, luz.batchwise_mask(batch, edge_index))
+
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    edge_index = torch.tensor([[0, 0, 0, 0, 1, 1, 2], [1, 2, 3, 4, 5, 6, 8]])
+    m = torch.tensor([[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]])
+    assert torch.allclose(m, luz.batchwise_mask(batch, edge_index))
+
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    edge_index = torch.tensor(
+        [
+            [0, 0, 0, 0, 1, 1, 2, 9, 10, 11, 12, 13],
+            [1, 2, 3, 4, 5, 6, 8, 10, 11, 12, 13, 14],
+        ]
+    )
+    m = torch.tensor(
+        [
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+        ]
+    )
+    assert torch.allclose(m, luz.batchwise_mask(batch, edge_index))
+
+
+def test_batchwise_edge_sum():
+    edges = torch.tensor(
+        [
+            [1.0, 1.0],
+            [2.0, 2.0],
+            [3.0, 3.0],
+            [4.0, 4.0],
+            [5.0, 5.0],
+            [6.0, 6.0],
+            [7.0, 7.0],
+        ]
+    )
+    edge_index = torch.tensor(
+        [[0, 1], [0, 2], [0, 3], [0, 4], [1, 5], [1, 6], [2, 8]]
+    ).T
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0])
+    s = torch.tensor([[28.0, 28.0]])
+    assert torch.allclose(s, luz.batchwise_edge_sum(edges, edge_index, batch))
+
+    edges = torch.tensor(
+        [
+            [1.0, 1.0],
+            [2.0, 2.0],
+            [3.0, 3.0],
+            [4.0, 4.0],
+            [5.0, 5.0],
+            [6.0, 6.0],
+            [7.0, 7.0],
+        ]
+    )
+    edge_index = torch.tensor(
+        [[0, 1], [0, 2], [0, 3], [0, 4], [1, 5], [1, 6], [2, 8]]
+    ).T
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    s = torch.tensor([[28.0, 28.0]])
+    assert torch.allclose(s, luz.batchwise_edge_sum(edges, edge_index, batch))
+
+    edges = torch.tensor(
+        [
+            [1.0, 1.0],
+            [2.0, 2.0],
+            [3.0, 3.0],
+            [4.0, 4.0],
+            [5.0, 5.0],
+            [6.0, 6.0],
+            [7.0, 7.0],
+            [8.0, 8.0],
+            [9.0, 9.0],
+            [10.0, 10.0],
+            [11.0, 11.0],
+            [12.0, 12.0],
+        ]
+    )
+    edge_index = torch.tensor(
+        [
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 4],
+            [1, 5],
+            [1, 6],
+            [2, 8],
+            [9, 10],
+            [10, 11],
+            [11, 12],
+            [12, 13],
+            [13, 14],
+        ]
+    ).T
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    s = torch.tensor([[28.0, 28.0], [50.0, 50.0]])
+    assert torch.allclose(s, luz.batchwise_edge_sum(edges, edge_index, batch))
+
+
+def test_batchwise_edge_mean():
+    edges = torch.tensor(
+        [
+            [1.0, 1.0],
+            [2.0, 2.0],
+            [3.0, 3.0],
+            [4.0, 4.0],
+            [5.0, 5.0],
+            [6.0, 6.0],
+            [7.0, 7.0],
+        ]
+    )
+    edge_index = torch.tensor(
+        [[0, 1], [0, 2], [0, 3], [0, 4], [1, 5], [1, 6], [2, 8]]
+    ).T
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0])
+    s = torch.tensor([[4.0, 4.0]])
+    assert torch.allclose(s, luz.batchwise_edge_mean(edges, edge_index, batch))
+
+    edges = torch.tensor(
+        [
+            [1.0, 1.0],
+            [2.0, 2.0],
+            [3.0, 3.0],
+            [4.0, 4.0],
+            [5.0, 5.0],
+            [6.0, 6.0],
+            [7.0, 7.0],
+        ]
+    )
+    edge_index = torch.tensor(
+        [[0, 1], [0, 2], [0, 3], [0, 4], [1, 5], [1, 6], [2, 8]]
+    ).T
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    s = torch.tensor([[4.0, 4.0]])
+    assert torch.allclose(s, luz.batchwise_edge_mean(edges, edge_index, batch))
+
+    edges = torch.tensor(
+        [
+            [1.0, 1.0],
+            [2.0, 2.0],
+            [3.0, 3.0],
+            [4.0, 4.0],
+            [5.0, 5.0],
+            [6.0, 6.0],
+            [7.0, 7.0],
+            [8.0, 8.0],
+            [9.0, 9.0],
+            [10.0, 10.0],
+            [11.0, 11.0],
+            [12.0, 12.0],
+        ]
+    )
+    edge_index = torch.tensor(
+        [
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 4],
+            [1, 5],
+            [1, 6],
+            [2, 8],
+            [9, 10],
+            [10, 11],
+            [11, 12],
+            [12, 13],
+            [13, 14],
+        ]
+    ).T
+    batch = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+    s = torch.tensor([[4.0, 4.0], [10.0, 10.0]])
+    assert torch.allclose(s, luz.batchwise_edge_mean(edges, edge_index, batch))
+
+
+def test_in_degree():
+    A = torch.tensor(
+        [
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 1, 0, 0, 0, 0],
+        ]
+    )
+
+    D_in = torch.tensor([0, 0, 3, 1, 1, 1, 1])
+
+    assert torch.allclose(D_in, luz.in_degree(A))
+
+    A = torch.tensor(
+        [
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [1, 1, 0, 1, 0, 0, 1],
+            [0, 0, 1, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 0, 1, 0],
+        ]
+    )
+
+    D_in = torch.tensor([1, 1, 4, 2, 2, 2, 2])
+
+    assert torch.allclose(D_in, luz.in_degree(A))
+
+
 def test_masked_softmax():
     mask = torch.tensor(
         [
@@ -31,3 +287,37 @@ def test_masked_softmax():
         luz.masked_softmax(x, mask, dim=1).sum(dim=1),
         torch.tensor([1.0, 1.0, 1.0, 1.0, 1.0]),
     )
+
+
+def test_out_degree():
+    A = torch.tensor(
+        [
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 1, 0, 0, 0, 0],
+        ]
+    )
+
+    D_out = torch.tensor([1, 1, 1, 1, 1, 1, 1])
+
+    assert torch.allclose(D_out, luz.out_degree(A))
+
+    A = torch.tensor(
+        [
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [1, 1, 0, 1, 0, 0, 1],
+            [0, 0, 1, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 0, 1, 0],
+        ]
+    )
+
+    D_out = torch.tensor([1, 1, 4, 2, 2, 2, 2])
+
+    assert torch.allclose(D_out, luz.out_degree(A))
