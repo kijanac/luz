8a576babb71c6bd5aef43cecd31bd7c96c8679f8
Ki-Jana Carter <kijana@mit.edu>
2021-01-10 22:42:50 -0500

    docs: Update docstrings.

--- src/luz/predictors.py
+++ src/luz/predictors.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional
+from typing import Any, Optional
 
 import copy
 import contextlib
@@ -32,24 +32,79 @@ class Predictor:
         return self.model(x)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Compute forward pass.
+
+        Parameters
+        ----------
+        x
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor.
+        """
         self.model.forward(x)
 
-    def predict(self, x: luz.Data) -> torch.Tensor:
-        with torch.no_grad(), self.eval():
+    def predict(self, x: torch.Tensor) -> torch.Tensor:
+        """Compute forward pass in eval mode.
+
+        Parameters
+        ----------
+        x
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor.
+        """
+        with self.eval():
             return self.__call__(x)
 
-    def to(self, *args, **kwargs) -> luz.Predictor:
+    def to(self, *args: Any, **kwargs: Any) -> luz.Predictor:
+        """Migrate to device.
+
+        Parameters
+        ----------
+        *args
+        **kwargs
+
+        Returns
+        -------
+        luz.Predictor
+            Migrated predictor.
+        """
         self.model.to(*args, **kwargs)
 
         return self
 
     def train(self, mode: Optional[bool] = True) -> luz.Predictor:
+        """Change training mode.
+
+        Parameters
+        ----------
+        mode
+            If True set to train mode, else set to eval mode; by default True.
+
+        Returns
+        -------
+        luz.Predictor
+            Predictor in appropriate mode.
+        """
         self.model.train(mode)
 
         return self
 
     @contextlib.contextmanager
     def eval(self, no_grad: Optional[bool] = True) -> None:
+        """Context manager to operate in eval mode.
+
+        Parameters
+        ----------
+        no_grad
+            If True use torch.no_grad(), by default True.
+        """
         training = copy.copy(self.model.training)
 
         nc = contextlib.nullcontext()
