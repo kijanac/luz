16effc2058b49de05a574162a8a84719df7c7bb2
Ki-Jana Carter <kijana@mit.edu>
2021-04-01 14:36:52 -0400

    test: Update tests.

---
+++ tests/test_scorers.py
@@ -0,0 +1,113 @@
+import luz
+import torch
+import unittest.mock as mock
+
+
+class IntegerDataset(luz.Dataset):
+    def __init__(self, n):
+        x = torch.arange(start=0, end=n, step=1, dtype=torch.float)
+        y = x ** 2
+        data = [luz.Data(x=_x, y=_y) for _x, _y in zip(x, y)]
+        super().__init__(data)
+
+
+class DummyModel(luz.Module):
+    def forward(self, x):
+        pass
+
+
+class DummyLearner(luz.Learner):
+    def learn(self, train_dataset, val_dataset=None, device="cpu"):
+        pass
+
+
+def test_cross_validation():
+    cv = luz.CrossValidation(num_folds=3, shuffle=False)
+
+    dataset = IntegerDataset(n=15)
+
+    m = DummyModel()
+    m.fit = mock.MagicMock(return_value=0.0)
+    m.test = mock.MagicMock(return_value=0.0)
+
+    learner = DummyLearner()
+    learner.learn = mock.MagicMock(return_value=m)
+
+    cv.score(learner, dataset, "cpu")
+    mock_calls = learner.learn.mock_calls
+
+    fold_inds = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14]]
+
+    assert len(mock_calls) == 4
+    for i, (name, args, kwargs) in enumerate(mock_calls[:-1]):
+        train_folds = kwargs["train_dataset"].datasets
+        train_fold_inds = (x for j, x in enumerate(fold_inds) if j != i)
+
+        assert len(train_folds) == 2
+        assert train_folds[0].indices == next(train_fold_inds)
+        assert train_folds[1].indices == next(train_fold_inds)
+
+    _, _, kwargs = learner.learn.mock_calls[-1]
+
+    assert len(kwargs["train_dataset"]) == 15
+
+    cv = luz.CrossValidation(num_folds=3, shuffle=False)
+
+    dataset = IntegerDataset(n=17)
+
+    m = DummyModel()
+    m.fit = mock.MagicMock(return_value=0.0)
+    m.test = mock.MagicMock(return_value=0.0)
+
+    learner = DummyLearner()
+    learner.learn = mock.MagicMock(return_value=m)
+
+    cv.score(learner, dataset, "cpu")
+    mock_calls = learner.learn.mock_calls
+
+    fold_inds = [[0, 1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11], [12, 13, 14, 15, 16]]
+
+    assert len(mock_calls) == 4
+    for i, (name, args, kwargs) in enumerate(mock_calls[:-1]):
+        train_folds = kwargs["train_dataset"].datasets
+        train_fold_inds = (x for j, x in enumerate(fold_inds) if j != i)
+
+        assert len(train_folds) == 2
+        assert train_folds[0].indices == next(train_fold_inds)
+        assert train_folds[1].indices == next(train_fold_inds)
+
+    _, _, kwargs = learner.learn.mock_calls[-1]
+
+    assert len(kwargs["train_dataset"]) == 17
+
+    cv = luz.CrossValidation(num_folds=6, shuffle=False)
+
+    dataset = IntegerDataset(n=17)
+
+    m = DummyModel()
+    m.fit = mock.MagicMock(return_value=0.0)
+    m.test = mock.MagicMock(return_value=0.0)
+
+    learner = DummyLearner()
+    learner.learn = mock.MagicMock(return_value=m)
+
+    cv.score(learner, dataset, "cpu")
+    mock_calls = learner.learn.mock_calls
+
+    fold_inds = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13, 14], [15, 16]]
+
+    assert len(mock_calls) == 7
+    for i, (name, args, kwargs) in enumerate(mock_calls[:-1]):
+        train_folds = kwargs["train_dataset"].datasets
+        train_fold_inds = (x for j, x in enumerate(fold_inds) if j != i)
+
+        assert len(train_folds) == 5
+        assert train_folds[0].indices == next(train_fold_inds)
+        assert train_folds[1].indices == next(train_fold_inds)
+        assert train_folds[2].indices == next(train_fold_inds)
+        assert train_folds[3].indices == next(train_fold_inds)
+        assert train_folds[4].indices == next(train_fold_inds)
+
+    _, _, kwargs = learner.learn.mock_calls[-1]
+
+    assert len(kwargs["train_dataset"]) == 17
---
+++ tests/test_tuners.py
@@ -0,0 +1,97 @@
+import luz
+import torch
+import unittest.mock as mock
+
+
+class IntegerDataset(luz.Dataset):
+    def __init__(self, n):
+        x = torch.arange(start=0, end=n, step=1, dtype=torch.float)
+        y = x ** 2
+        data = [luz.Data(x=_x, y=_y) for _x, _y in zip(x, y)]
+        super().__init__(data)
+
+
+class DummyModel(luz.Module):
+    def forward(self, x):
+        pass
+
+
+class DummyLearner(luz.Learner):
+    def learn(self, train_dataset, val_dataset=None, device="cpu"):
+        pass
+
+
+def test_random_search():
+    dataset = IntegerDataset(n=15)
+
+    m = DummyModel()
+    m.fit = mock.MagicMock(return_value=0.0)
+    m.test = mock.MagicMock(return_value=0.0)
+
+    learner = DummyLearner()
+    learner.learn = mock.MagicMock(return_value=m)
+
+    scorer = luz.Holdout(test_fraction=0.2)
+
+    tuner = luz.RandomSearch(5, scorer)
+
+    hps = []
+
+    old_seed = torch.random.initial_seed()
+    for exp in tuner.tune(x=tuner.choose(1, 2, 3, 4, 5), y=tuner.sample(1, 4)):
+        assert torch.random.initial_seed() == 0
+        tuner.score(learner, dataset, "cpu")
+        hps.append((exp.x, exp.y))
+
+    assert torch.random.initial_seed() == old_seed
+
+    for x, y in hps:
+        assert x in [1, 2, 3, 4, 5]
+        assert y >= 1 and y < 4
+
+    assert sum(len(s) for s in tuner.scores.values()) == 5
+
+
+def test_grid_search():
+    dataset = IntegerDataset(n=15)
+
+    m = DummyModel()
+    m.fit = mock.MagicMock(return_value=0.0)
+    m.test = mock.MagicMock(return_value=0.0)
+
+    learner = DummyLearner()
+    learner.learn = mock.MagicMock(return_value=m)
+
+    scorer = luz.Holdout(test_fraction=0.2)
+
+    tuner = luz.GridSearch(scorer)
+
+    hps = []
+
+    old_seed = torch.random.initial_seed()
+    for exp in tuner.tune(x=tuner.choose(1, 2, 3, 4, 5), y=tuner.choose(1, 2, 3)):
+        assert torch.random.initial_seed() == 0
+        tuner.score(learner, dataset, "cpu")
+        hps.append((exp.x, exp.y))
+
+    assert torch.random.initial_seed() == old_seed
+
+    assert sum(len(s) for s in tuner.scores.values()) == 15
+
+    assert hps == [
+        (1, 1),
+        (1, 2),
+        (1, 3),
+        (2, 1),
+        (2, 2),
+        (2, 3),
+        (3, 1),
+        (3, 2),
+        (3, 3),
+        (4, 1),
+        (4, 2),
+        (4, 3),
+        (5, 1),
+        (5, 2),
+        (5, 3),
+    ]
