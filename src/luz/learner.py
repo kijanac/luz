cc879e5a0586800afe87fb1af290cd2fc25e96ac
Ki-Jana Carter <kijana@mit.edu>
2021-05-02 18:42:08 -0400

    feat: Add new features to Learner to reduce boilerplate code and simplify scoring and tuning

--- src/luz/learner.py
+++ src/luz/learner.py
@@ -12,9 +12,33 @@ Device = Union[str, torch.device]
 
 class Learner(ABC):
     @abstractmethod
+    def model(self):
+        pass
+
+    @abstractmethod
+    def fit_params(self, train_dataset, val_dataset, device):
+        pass
+
+    def hyperparams(self):
+        raise NotImplementedError
+
+    def score(self, dataset, device: Optional[Device] = "cpu"):
+        return self.scorer.score(self, dataset, device)
+
+    def tune(self, dataset, device: Optional[Device] = "cpu"):
+        for exp in self.tuner.tune(**self.hyperparams()):
+            self.use_hyperparams(exp)
+            model, score = self.tuner.score(self, dataset, device)
+
+        return (
+            self.tuner.best_model,
+            self.tuner.best_score,
+            self.tuner.best_hyperparameters,
+        )
+
     def learn(
         self,
-        dataset: luz.Dataset,
+        train_dataset: luz.Dataset,
         val_dataset: Optional[luz.Dataset] = None,
         device: Optional[Device] = "cpu",
     ) -> luz.Module:
@@ -22,7 +46,7 @@ class Learner(ABC):
 
         Parameters
         ----------
-        dataset
+        train_dataset
             Training dataset used to learn a model.
         val_dataset
             Validation dataset, by default None.
@@ -34,4 +58,20 @@ class Learner(ABC):
         luz.Module
             Learned model.
         """
-        pass
+        model = self.model()
+
+        fit_params = self.fit_params(train_dataset, val_dataset, device)
+        model.use_fit_params(**fit_params)
+
+        model.fit(dataset=train_dataset, val_dataset=val_dataset, device=device)
+
+        return model
+
+    def use_scorer(self, scorer):
+        self.scorer = scorer
+
+    def use_tuner(self, tuner):
+        self.tuner = tuner
+
+    def use_hyperparams(self, hyperparams):
+        self.hyperparams = hyperparams
